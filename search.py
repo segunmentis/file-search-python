03324ecdfc731fa248ef3718a443c002cad87a3a
segunmentis <mentis2010@gmail.com>
2021-01-23 16:52:31 -0600

    Exclude image files from search

--- search.py
+++ search.py
@@ -1,5 +1,16 @@
 from os import path
 import termcolor
+import pathlib
+
+excluded_extensions = [".jpg", ".png", ".jpeg", ".gif"]
+
+
+def check_file_extension(filename):
+    file_extension = pathlib.Path(filename).suffix
+    if file_extension in excluded_extensions:
+        return True
+    else:
+        return False
 
 
 def search_file(name, word):
@@ -26,5 +37,9 @@ if __name__ == "__main__":
     file_name = input("Enter the file name: ")
     while not path.exists(file_name):
         file_name = input("The file does not exist, enter the file name again: ")
+    excluded = check_file_extension(file_name)
+    if excluded:
+        print("The input file cannot be an image file, please try again with another file.")
+        exit()
     search_string = input("Enter search string: ")
     search_file(file_name, search_string)
